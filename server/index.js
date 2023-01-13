6bb5a2b42c6f9dc07db2a7e62b0bfafe33250fda
Maddie Sime <madisonfaithsime@gmail.com>
2023-01-13 10:54:23 -0800

    Set up Express server, created a bare-bones HTML file in the client/dist folder, created a .gitignore file, edited package.json to include express, nodemon, and a script to run the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-// Add data
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// basic middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// will need to get webpack up and running as well as our top level React component in order to see anything in the browser
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// basic route to get us started
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// connection (we will use the standard localhost:3000 as our development environment)
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
