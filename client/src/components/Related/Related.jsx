e00de505e261ab72a2df2ea5c3c042868ac48a66
Maddie Sime <madisonfaithsime@gmail.com>
2023-01-13 15:29:29 -0800

    Set up basic "Related" React components, added react, react-dom, and axios to package.json

---
+++ client/src/components/Related/Related.jsx
@@ -0,0 +1,9 @@
+// The Related component will display both the "related products" carousel and the "my outfit" carousel
+import React from 'react';
+import Card from './Card.jsx';
+
+var Related = () => {
+
+};
+
+export default Related;
