f6fe219a6aacd149449afc856b36bb71a5a06ef0
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-01-14 16:06:11 -0500

    Added missing closing quote

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Overview from "./Overview/Overview.jsx";
-import QandA from "./Q&A/Q&A.jsx;
+import QandA from "./Q&A/Q&A.jsx";
 import Ratings from "./Ratings/Ratings.jsx";
 import Related from "./Related/Related.jsx";
 
