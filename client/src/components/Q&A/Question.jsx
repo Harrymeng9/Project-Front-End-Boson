e026eb197a263aab721e3b816f7ee947fb399ae4
fig <arielfishkin@gmail.com>
2023-01-13 18:49:36 -0800

    added component files for `Question` and `QuestionsList`

---
+++ client/src/components/Q&A/Question.jsx
@@ -0,0 +1,14 @@
+/*
+
+
+
+/*/
+
+
+//render "Q:" concatenated with the actual question string
+//render "A:" followed by list of answers. Each answer is rendered in the format...
+    //body text of the answer
+    //(new line) name of answerer and date answered - “[username], Month DD, YYYY”
+      //username is bolded and reads "Seller" if the answer was written by seller
+    //same line as above:
+    //"Helpful? Yes (#)" where # is how many users have clicked yes
