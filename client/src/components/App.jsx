ceaaaa424cad1d8186eab3d41030ed6853a7f43b
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-01-14 15:48:37 -0500

    Create a barebones App for housing components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,1 +1,20 @@
-// Add data
+import React from "react";
+import Overview from "./Overview/Overview.jsx";
+import QandA from "./Q&A/Q&A.jsx;
+import Ratings from "./Ratings/Ratings.jsx";
+import Related from "./Related/Related.jsx";
+
+var App = () => {
+  //add state data as needed here
+
+  //add other functions as needed here
+
+  return (
+    <div>Overview</div>
+    <div>QandA</div>
+    <div>Ratings</div>
+    <div>Related</div>
+  )
+}
+
+export default App;
