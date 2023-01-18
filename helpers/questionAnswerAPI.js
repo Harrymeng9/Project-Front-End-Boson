aa9e45024e9eafd9b528199862eb273fc2ab8c33
fig <arielfishkin@gmail.com>
2023-01-17 16:54:20 -0800

    added functionality for fetching questions into useEffect in index.js

--- helpers/questionAnswerAPI.js
+++ helpers/questionAnswerAPI.js
@@ -6,18 +6,30 @@ const axios = require('axios');
 
 //create helper method for retrieving a sorted list of questions from the API
 var fetchQuestions = (productId, resultCount) => {
-  //make a get request to the Atelier API using axios
-  axios({
-    //specify method
-    method: 'get',
-    //specify url
-    url: `http://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions?product_id=${productId}&count=${resultCount}`,
-    //include authorization headers
-    headers: {
-      authorization: `${process.env.TOKEN}`
-    }
+  return new Promise((resolve, reject) => {
+    //make a get request to the Atelier API using axios
+    axios({
+      //specify method
+      method: 'get',
+      //specify url
+      url: `http://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions?product_id=${productId}&count=${resultCount}`,
+      //include authorization headers
+      headers: {
+        authorization: `${process.env.TOKEN}`
+      }
+    })
+      .then((results) => {
+        console.log(results);
+        resolve(results);
+      })
+      .catch((err) => {
+        console.log('error in fetchQuestions helper');
+        reject(err);
+      });
   });
+
 };
 
+module.exports.fetchQuestions = fetchQuestions;
 
 
