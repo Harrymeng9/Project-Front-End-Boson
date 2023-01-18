25bbee03683f86ca710cfb1880b35b7f2be5f47d
fig <arielfishkin@gmail.com>
2023-01-17 16:17:28 -0800

    fetch function stub

--- helpers/questionAnswerAPI.js
+++ helpers/questionAnswerAPI.js
@@ -4,3 +4,16 @@ const axios = require('axios');
 //this file will contain the functions for making requests to the API for
 //question and answer data
 
+//create helper method for retrieving a sorted list of questions from the API
+var fetchQuestions = (productId, resultCount) => {
+  //make a get request to the Atelier API using axios
+  axios({
+    //specify method
+    method: 'get',
+    //specify url
+    url: `http://www.api.com/page?product_id=${productId}&count=${resultCount}`,
+    //include authorization headers
+  });
+};
+
+
