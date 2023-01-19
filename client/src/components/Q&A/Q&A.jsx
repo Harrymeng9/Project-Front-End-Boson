1c34792a1202b7d487b9b93bbee273d74ae94ddf
fig <arielfishkin@gmail.com>
2023-01-18 20:51:22 -0800

    fixed `React` import capitalization

--- client/src/components/Q&A/Q&A.jsx
+++ client/src/components/Q&A/Q&A.jsx
@@ -1,5 +1,4 @@
-import react from 'react';
-import reactDOM from 'react-dom';
+import React from 'react';
 import QuestionList from './qaComponents/QuestionList.jsx';
 import AnswerList from './qaComponents/AnswerList.jsx';
 import SearchQuestions from './qaComponents/SearchQuestions.jsx';
--- client/src/components/Q&A/qaComponents/AnswerList.jsx
+++ client/src/components/Q&A/qaComponents/AnswerList.jsx
@@ -1,4 +1,4 @@
-import react from 'react';
+import React from 'react';
 import Answer from './Answer.jsx';
 
 var AnswerList = (props) => {
