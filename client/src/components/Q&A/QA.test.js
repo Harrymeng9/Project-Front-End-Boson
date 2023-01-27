879694c9edae98044d3d5069a555d8e07a9f2751
fig <arielfishkin@gmail.com>
2023-01-27 13:42:25 -0800

    added `jest-environment-jsdom` into npm modules; component test stub

--- client/src/components/Q&A/QA.test.js
+++ client/src/components/Q&A/QA.test.js
@@ -1,9 +1,23 @@
-import { renderQuestions } from "./qaComponents/QuestionList.jsx"
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { renderQuestions } from "./qaComponents/QuestionList.jsx";
+import { render, screen, getByText } from '@testing-library/react';
+import QuestionAndAnswer from "./Q&A.jsx";
 
-describe("Integration Tests", function () {
+/** @jest-environment jsdom */
+
+describe("Function Tests", function () {
   var testArray = [{question_id: 1, question_body: 'hello', answers: {}}, {question_id: 2, question_body: 'goodbye', answers: {}}];
   test("Should produce an array equal in length to the input array", () => {
     var resultArr = renderQuestions(testArray);
     expect(resultArr.length).toEqual(testArray.length);
+  });
+});
+
+describe("Component Tests", function () {
+  test("Renders correct title on Parent Q&A component", () => {
+    var title = "QUESTIONS AND ANSWERS";
+    render(<QuestionAndAnswer />);
+    expect(screen.getByText(title)).toBe(true);
   });
 });
