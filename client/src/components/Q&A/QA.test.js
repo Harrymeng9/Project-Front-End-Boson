2806a3cada5561fa3f2d5bfd9a372c4df161d114
fig <arielfishkin@gmail.com>
2023-01-27 16:23:46 -0800

    added jest environment node modules and imported into QA test file

--- client/src/components/Q&A/QA.test.js
+++ client/src/components/Q&A/QA.test.js
@@ -1,10 +1,12 @@
+/** @jest-environment jsdom */
+
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import '@testing-library/jest-dom';
 import { renderQuestions } from "./qaComponents/QuestionList.jsx";
 import { render, screen, getByText } from '@testing-library/react';
 import QuestionAndAnswer from "./Q&A.jsx";
 
-/** @jest-environment jsdom */
 
 describe("Function Tests", function () {
   var testArray = [{question_id: 1, question_body: 'hello', answers: {}}, {question_id: 2, question_body: 'goodbye', answers: {}}];
@@ -18,6 +20,6 @@ describe("Component Tests", function () {
   test("Renders correct title on Parent Q&A component", () => {
     var title = "QUESTIONS AND ANSWERS";
     render(<QuestionAndAnswer />);
-    expect(screen.getByText(title)).toBe(true);
+    expect(screen.getByText(title)).toBe("<h4>QUESTIONS AND ANSWERS</h4>");
   });
 });
