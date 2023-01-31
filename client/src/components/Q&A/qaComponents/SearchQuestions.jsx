a68d093d88b9787b42d7012f4d6153ddc7db446c
fig <arielfishkin@gmail.com>
2023-01-30 21:03:22 -0800

    skeleton for search component with handle form change

--- client/src/components/Q&A/qaComponents/SearchQuestions.jsx
+++ client/src/components/Q&A/qaComponents/SearchQuestions.jsx
@@ -2,11 +2,50 @@ import React from 'react';
 
 var SearchQuestions = (props) => {
 
+  var handleChange = (e) => {
+    props.setTerm(e.target.value)
+    //create empty result array
+    var filtered = [];
+    if (props.term.length > 2) {
+      //if search term is at least 3 chars long...
+      //iterate over the questions array
+        //if the question body contains the term
+          //question.question_body.includes(props.term)
+          //push the current question to the array
+        //otherwise iterate over the answers object
+          //if the answer body contains the term
+            //answer.answerId.body
+          //push the current question to the array
+    }
+    //set questions state equal to the filtered array
+  }
+
   return (
     <div>
-      Search Questions Component
+      <form>
+        <input onChange={handleChange}
+        type="text"
+        name="search"
+        placeholder="Have a question? Search for answers…"
+        value={props.term}
+        />
+      </form>
     </div>
   )
 }
 
 export default SearchQuestions;
+
+
+//# A search bar will appear above the questions list.
+//Search terms entered in this text input will filter the list for matching results.
+
+//After the user types 3 or more characters into the search bar the results will
+  //begin to filter to only those containing matching text.
+//The filter should continue to update as the user adds or removes characters.
+//# The bar should display placeholder text reading “Have a question? Search for answers…”
+//If the user clears the search term, or removes characters so that less than 3 remain,
+//the list should return to the state where it is not filtered to matching text.
+//The search filter should work with any other filters or sorts that have been applied,
+//and narrow the results further.
+//Changes to the sort and rating filters should not remove the search term filter.
