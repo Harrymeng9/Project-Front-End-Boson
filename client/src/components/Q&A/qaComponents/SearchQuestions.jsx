6b56d89013a649e2ade8494c0ea961373b697919
fig <arielfishkin@gmail.com>
2023-01-30 19:59:03 -0800

    added notes on search function requirements

--- client/src/components/Q&A/qaComponents/SearchQuestions.jsx
+++ client/src/components/Q&A/qaComponents/SearchQuestions.jsx
@@ -10,3 +10,17 @@ var SearchQuestions = (props) => {
 }
 
 export default SearchQuestions;
+
+
+//A search bar will appear above the questions list.
+//Search terms entered in this text input will filter the list for matching results.
+
+//After the user types 3 or more characters into the search bar the results will
+//begin to filter to only those containing matching text.
+//The filter should continue to update as the user adds or removes characters.
+//The bar should display placeholder text reading “Have a question? Search for answers…”
+//If the user clears the search term, or removes characters so that less than 3 remain,
+//the list should return to the state where it is not filtered to matching text.
+//The search filter should work with any other filters or sorts that have been applied,
+//and narrow the results further.   Changes to the sort and rating
+//filters should not remove the search term filter.
