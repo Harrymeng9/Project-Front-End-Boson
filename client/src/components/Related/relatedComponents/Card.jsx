56cd0d9ca094073a765e75ab888463b9907ec67e
Maddie Sime <madisonfaithsime@gmail.com>
2023-01-30 20:48:22 -0800

    Can now see the features to be compared in the overlay modal

--- client/src/components/Related/relatedComponents/Card.jsx
+++ client/src/components/Related/relatedComponents/Card.jsx
@@ -1,17 +1,65 @@
 // The Card component will be used for both the "related products" cards and the "my outfit" cards
 import React from "react";
 import { useState } from "react";
+import axios from 'axios';
 
 var Card = (props) => {
   /* will need: product category, product name, price, star rating,
   product preview image, an x or a star in top right corner, depending
   on which type of card (related product or outfit) */
 
+  var handleStarButtonClick = () => {
+    props.setStarButtonClick(!props.starButtonClick);
+    var selectedRelatedProduct = props.productId;
+
+    axios.get(`/products/${props.currentProduct}`)
+      .then((results) => {
+        var features = results.data.features;
+        var currentProductFeatures = [];
+        for (var i = 0; i < features.length; i++) {
+          var feature = features[i].feature;
+          var value = features[i].value;
+          if (value === null) {
+            currentProductFeatures.push(feature);
+          } else if (feature === null) {
+            currentProductFeatures.push(value);
+          } else if (feature !== null && value !== null) {
+            currentProductFeatures.push(value + ' ' + feature);
+          }
+        }
+        props.setCurrentProductFeatures(currentProductFeatures);
+      })
+      .catch((error) => {
+        console.log('There is an error in Card.jsx when trying to get current product features', error);
+      })
+      .then(() => {
+        return axios.get(`/products/${selectedRelatedProduct}`)
+      })
+      .then((results) => {
+        var features = results.data.features;
+        var selectedRelatedProductFeatures = [];
+        for (var i = 0; i < features.length; i++) {
+          var feature = features[i].feature;
+          var value = features[i].value;
+          if (value === null) {
+            selectedRelatedProductFeatures.push(feature);
+          } else if (feature === null) {
+            selectedRelatedProductFeatures.push(value);
+          } else if (feature !== null && value !== null) {
+            selectedRelatedProductFeatures.push(value + ' ' + feature);
+          }
+        }
+        props.setSelectedRelatedProductFeatures(selectedRelatedProductFeatures);
+      })
+      .catch((error) => {
+        console.log('There is an error in Card.jsx when trying to get selected related product features', error);
+      })
+  }
 
   return (
     <div className="card">
       <div className="related-image-container">
-        <button onClick={() => { props.setStarButtonClick(!props.starButtonClick); }} className="related-image-button">Star</button>
+        <button onClick={handleStarButtonClick} className="related-image-button">Star</button>
         <img className="related-image" src={props.image}></img>
       </div>
       <div>
