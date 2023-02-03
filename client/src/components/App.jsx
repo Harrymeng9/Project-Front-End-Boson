7b75e940b135b54cc1ecf6083d05b176befad8f7
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-02-02 21:52:36 -0500

    Create a basic HOC for overview

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,24 +1,41 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import Overview from "./Overview/Overview.jsx";
-import QuestionAndAnswer from "./Q&A/Q&A.jsx";
-import Ratings from "./Ratings/Ratings.jsx";
-import { Related } from "./Related/Related.jsx";
-import axios from 'axios';
-import ErrorBoundary from '../Utils/ErrorBoundary.jsx';
+  import React from "react";
+  import { useState, useEffect } from "react";
+  import Overview from "./Overview/Overview.jsx";
+  import QuestionAndAnswer from "./Q&A/Q&A.jsx";
+  import Ratings from "./Ratings/Ratings.jsx";
+  import { Related } from "./Related/Related.jsx";
+  import axios from 'axios';
+  import ErrorBoundary from '../Utils/ErrorBoundary.jsx';
 
-var App = () => {
-  //add state data as needed here
-  const [productId, setProductId] = useState(71698)
+  const withConditionalFeedback = (Component) => (props) => {
+    console.log('withcomp', Component)
+    console.log('withprops', props)
+    if (!props) {return <div>Data is empty.</div>;}
+    else {return <Component {...props} />;}
+  };
 
-  return (
-    <div>
-      <div className="overviewMain"><Overview initial={productId} /></div>
-      <div><Related productId={productId} setProductId={setProductId} /></div>
-      <div><QuestionAndAnswer productId={productId} /></div>
-      <div><Ratings productId={productId} /></div>
-    </div>
-  )
-}
+  const baseOverview = ({data}) => {
+    console.log(arguments)
+    console.log('wearehere', {data});
+    return (
+      <div className="overviewMain"><Overview initial={data}/></div>
+    );
+  };
 
-export default App;
+  const OverviewRender = withConditionalFeedback(baseOverview);
+
+  var App = () => {
+    //add state data as needed here
+    const [productId, setProductId] = useState(71701)
+
+    return (
+      <div>
+        <OverviewRender data={productId}/>
+        <div><Related productId={productId} setProductId={setProductId} /></div>
+        <div><QuestionAndAnswer productId={productId} /></div>
+        <div><Ratings productId={productId} /></div>
+      </div>
+    )
+  }
+
+  export default App;
