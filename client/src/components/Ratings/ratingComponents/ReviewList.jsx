151613b89b8abbeb00890010222e17d32d7bf5ed
HarryMeng <harry.mld.2010@gmail.com>
2023-02-05 16:54:38 -0800

    Update the way to get API data due to API errors (seems like); Able to sort by relevant/newest/helpful;

--- client/src/components/Ratings/ratingComponents/ReviewList.jsx
+++ client/src/components/Ratings/ratingComponents/ReviewList.jsx
@@ -8,44 +8,40 @@ var ReviewList = (props) => {
 
   const [reviewsList, setReviewsList] = useState([]);
   const [modal, setModal] = useState(false);
-  const [page, setPage] = useState(1);
-  const [newReviews, setNewReviews] = useState([]);
   const [sort, setSort] = useState('relevant');
+  const [commentedReviews, setCommentedReviews] = useState({});
+  const [renderCount, setRenderCount] = useState(2);
 
-  var fetchReviews = () => {
-    axios.get('/reviews', { params: { page: page, sort: sort, product_id: props.product_id } })
+  useEffect(() => {
+    if (props.totalReviews > 0) {
+      axios.get('/reviews', { params: { count: props.totalReviews, sort: sort, product_id: props.product_id } })
+        .then((data) => {
+          setReviewsList(data.data.results);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+  }, []);
+
+  // Once sort option changes, retrieve the data from API
+  useEffect(() => {
+    axios.get('/reviews', { params: { count: props.totalReviews, sort: sort, product_id: props.product_id } })
       .then((data) => {
-        setNewReviews(data.data.results);
-        setReviewsList(reviewsList.concat(data.data.results));
+        setReviewsList(data.data.results);
+        // Reset the renderCount state to show 2 items only
+        setRenderCount(2);
       })
       .catch((err) => {
         console.log(err);
       });
-  };
-
-  useEffect(() => {
-    fetchReviews();
-  }, []);
-
-  useEffect(() => {
-    fetchReviews();
-  }, [page])
-
-  // Once sort option changes, retrieve the data from API
-  useEffect(()=> {
-    axios.get('/reviews', { params: { page: page, sort: sort, product_id: props.product_id } })
-    .then((data) => {
-      setReviewsList(data.data.results);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
   }, [sort]);
 
   if (reviewsList) {
     // Return a list of reviews for a particular product
     return (
       <div id='reviewList'>
+        {/* Sorting */}
         <div style={{ display: 'flex' }}>
           <div className='sort-options'>Sort on: </div>
           <div>
@@ -56,25 +52,30 @@ var ReviewList = (props) => {
             </select>
           </div>
         </div>
+        {/* Each Review Tile */}
         <div style={{ maxHeight: '800px', overflow: 'auto' }}>
           {
-            reviewsList.map((result, i) => {
+            reviewsList.slice(0, renderCount).map((result, i) => {
               return (
                 <div key={i}>
-                  <ReviewTile review={result} />
+                  <ReviewTile review={result} photos={result.photos} review_id={result.review_id} commentedReviews={commentedReviews} setCommentedReviews={setCommentedReviews} />
                 </div>
               )
             })
           }
         </div>
-        {newReviews.length === 2 && <button onClick={(e) => setPage(page + 1)}>MORE REVIEWS</button>}
+        {/* More Review Button */}
+        {renderCount <= reviewsList.length && <button onClick={(e) => setRenderCount(renderCount + 2)}>MORE REVIEWS</button>}
+        {/* Add a Review Button */}
         <button onClick={(e) => setModal(!modal)}>ADD A REVIEW + </button>
         {modal && <Modal setModal={setModal} modal={modal} product_id={props.product_id} productChars={props.productChars} />}
       </div >
     )
   } else {
     return (
-      <div></div>
+      <div>
+        <button onClick={(e) => setModal(!modal)}>ADD A REVIEW + </button>
+      </div>
     )
   }
 }
