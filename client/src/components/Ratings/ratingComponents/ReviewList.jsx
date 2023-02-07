6acf512a18f4deed5b51782a0613bce30dd7626c
HarryMeng <harry.mld.2010@gmail.com>
2023-02-06 21:15:23 -0800

    Update the functionality for stars filter; Filter By will come up once filters are selected and also the sentence Remove All Filters with link;

--- client/src/components/Ratings/ratingComponents/ReviewList.jsx
+++ client/src/components/Ratings/ratingComponents/ReviewList.jsx
@@ -11,12 +11,15 @@ var ReviewList = (props) => {
   const [sort, setSort] = useState('relevant');
   const [commentedReviews, setCommentedReviews] = useState({});
   const [renderCount, setRenderCount] = useState(2);
+  const [filteredReviewListToRender, setFilteredReviewListToRender] = useState([]);
 
   useEffect(() => {
     if (props.totalReviews > 0) {
       axios.get('/reviews', { params: { count: props.totalReviews, sort: sort, product_id: props.product_id } })
         .then((data) => {
+          // console.log('data', data.data.results);
           setReviewsList(data.data.results);
+          setFilteredReviewListToRender(data.data.results);
         })
         .catch((err) => {
           console.log(err);
@@ -24,11 +27,28 @@ var ReviewList = (props) => {
     };
   }, []);
 
+  useEffect(() => {
+    var copyReviewList = reviewsList.slice();
+    var filteredReviewList = [];
+
+    if (!props.filterStars[1] && !props.filterStars[2] && !props.filterStars[3] && !props.filterStars[4] && !props.filterStars[5]) {
+      filteredReviewList = copyReviewList.slice();
+    } else {
+      for (var i = 0; i < copyReviewList.length; i++) {
+        if (props.filterStars[copyReviewList[i].rating]) {
+          filteredReviewList.push(copyReviewList[i]);
+        }
+      }
+    }
+    setFilteredReviewListToRender(filteredReviewList);
+  }, [props.filterStars]);
+
   // Once sort option changes, retrieve the data from API
   useEffect(() => {
     axios.get('/reviews', { params: { count: props.totalReviews, sort: sort, product_id: props.product_id } })
       .then((data) => {
         setReviewsList(data.data.results);
+        setFilteredReviewListToRender(data.data.results);
         // Reset the renderCount state to show 2 items only
         setRenderCount(2);
       })
@@ -55,17 +75,19 @@ var ReviewList = (props) => {
         {/* Each Review Tile */}
         <div style={{ maxHeight: '800px', overflow: 'auto' }}>
           {
-            reviewsList.slice(0, renderCount).map((result, i) => {
+            filteredReviewListToRender.slice(0, renderCount).map((result, i) => {
               return (
                 <div key={i}>
                   <ReviewTile review={result} photos={result.photos} review_id={result.review_id} commentedReviews={commentedReviews} setCommentedReviews={setCommentedReviews} />
                 </div>
               )
-            })
+            }
+              // }
+            )
           }
         </div>
         {/* More Review Button */}
-        {renderCount <= reviewsList.length && <button onClick={(e) => setRenderCount(renderCount + 2)}>MORE REVIEWS</button>}
+        {renderCount <= filteredReviewListToRender.length && <button onClick={(e) => setRenderCount(renderCount + 2)}>MORE REVIEWS</button>}
         {/* Add a Review Button */}
         <button onClick={(e) => setModal(!modal)}>ADD A REVIEW + </button>
         {modal && <Modal setModal={setModal} modal={modal} product_id={props.product_id} productChars={props.productChars} />}
