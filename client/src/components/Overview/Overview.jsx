57d913fa3e8fecb31900813249436650f51cf678
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-02-09 20:10:08 -0500

    Clean overview's state rendering, and add the basic carousel functionalities

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -5,29 +5,16 @@ import InfoList from './ovComponents/infoList.jsx';
 import StyleSelect from './ovComponents/styleSelect.jsx';
 import GalleryCarousel from './ovComponents/galleryCarousel.jsx';
 
-var truth = false;
-
 var Overview = (props) => {
   //add the useState parameters here
-  const [SKUS, setProds] = useState([]);
-  const [skuInfo, setInfo] = useState([]);
-  const [currentStyle, setStyle] = useState([]);
-  const [related, setRelated] = useState([]);
   const [initial, setInitial] = useState([]);
   const [intStyle, setIntStyle] = useState([]);
   const [review, setReviews] = useState(0)
   const [bigPic, setBigPic] = useState('')
   const [windowPic, setWindowPic] = useState('');
+  const [urlArray, setURLArray] = useState([]);
 
   // //add the axios get here
-  var productGet = () => {
-    axios.get('/products')
-      .then(info => {
-        setProds(info.data);
-      })
-      .catch(err => console.log(err))
-  }
-
   // first item to render //
   var initialFetch = () => {
     return axios.get(`/products/${props.initial}`)
@@ -40,9 +27,15 @@ var Overview = (props) => {
   var firstStyle = () => {
     return axios.get(`/products/${props.initial}/styles`)
       .then(data => {
+        var localUrlArray = [];
         var firstPic = data.data.results[0].photos[0].thumbnail_url;
         var firstWindow = data.data.results[0].photos[0].url;
+        var firstUrls = data.data.results[0].photos;
         if (!firstPic) {setBigPic('No pictures to show')}
+        firstUrls.forEach(img => {
+          localUrlArray.push(img.url)
+        })
+        setURLArray(localUrlArray)
         setBigPic(firstPic)
         setIntStyle(data.data.results);
         setWindowPic(firstWindow);
@@ -61,69 +54,10 @@ var Overview = (props) => {
 
 
   ///////////////////////////////////////////
-  // single item //
-  var infoFetcher = (id) => {
-    return axios.get(`/products/${id}`)
-      .then(data => {
-        return data.data;
-      })
-      .catch(err => console.log(err));
-  }
-
-  let setter = async () => {
-    var saver = [];
-    for (var i = 0; i < SKUS.length; i++) {
-      await infoFetcher(SKUS[i].id)
-        .then(a => {
-          saver.push(a);
-        })
-    }
-    setInfo(saver);
-  }
-
-  // single style //
-  var styleFetcher = (id) => {
-    return axios.get(`/products/${id}/styles`)
-      .then(data => {
-        return data.data;
-      })
-      .catch(err => console.log(err));
-  }
-
-  let styler = async () => {
-    var saver = [];
-    for (var i = 0; i < SKUS.length; i++) {
-      await styleFetcher(SKUS[i].id)
-        .then(a => {
-          saver.push(a);
-        })
-    }
-    setStyle(saver);
-  }
-
-  // related items//
-  var relatedFetcher = (id) => {
-    return axios.get(`/products/${id}/related`)
-      .then(data => {
-        return data.data;
-      })
-      .catch(err => console.log(err));
-  }
-
-  let relations = async () => {
-    var saver = [];
-    for (var i = 0; i < SKUS.length; i++) {
-      await relatedFetcher(SKUS[i].id)
-        .then(a => {
-          saver.push(a);
-        })
-    }
-    setRelated(saver);
-  }
-
   // click function to set the style as the first/default style
   let sets = (styleID) => {
     var redone = [];
+    var updateUrlArray = [];
     var copy = intStyle.slice(0)
     for (var i = 0; i < copy.length; i++) {
       if (copy[i].style_id === styleID) {
@@ -135,32 +69,22 @@ var Overview = (props) => {
     copy.forEach(e => {
       redone.push(e);
     })
+    console.log('ovred', redone[0].photos)
+    var updatedPics = redone[0].photos;
+    updatedPics.forEach(img => {
+      updateUrlArray.push(img.url);
+    })
     setIntStyle(redone);
     setWindowPic(`${redone[0].photos[0].url}`)
+    setURLArray(updateUrlArray)
   }
 
   // create a function that calls the axios requests on load
-  let loader = () => {
-    //checks and runs the setter function on page render only once
-    if (truth === false) {
-      setter();
-      styler();
-      relations();
-      initialFetch();
-      firstStyle();
-      reviews();
-      truth = true;
-    }
-    //returns the components when finally called
-    return (
-      <div>
-        {/* <div><img className="mainPic" src={bigPic} onClick={(e) => window.open(windowPic)}></img></div> */}
-        <div><GalleryCarousel firstWindowPic={windowPic} setWindowPic={setWindowPic}/></div>
-        <div>{<InfoList info={initial} styles={currentStyle} ratings={review}/>}</div>
-        <StyleSelect styles={intStyle} id={props.initial} clickfunc={sets} setBigPic={setBigPic} setWindowPic={setWindowPic}/>
-      </div>
-    )
-  }
+  useEffect(() => {
+    initialFetch();
+    firstStyle();
+    reviews();
+  }, [props.initial])
 
   //this is to crate a faux loading screen while the state is being set.
   //if the state is set, then we can fully render the app
@@ -169,7 +93,15 @@ var Overview = (props) => {
       <div>Loading your products</div>
     )
   } else {
-    return loader();
+    return (
+      <div>
+        {/* <div><img className="mainPic" src={bigPic} onClick={(e) => window.open(windowPic)}></img></div> */}
+        <div><GalleryCarousel firstWindowPic={windowPic} setWindowPic={setWindowPic} urlArray={urlArray}/></div>
+        <div>{<InfoList info={initial} atings={review}/>}</div>
+        <StyleSelect styles={intStyle} id={props.initial} clickfunc={sets}
+        setBigPic={setBigPic} setWindowPic={setWindowPic} setURLArray={setURLArray}/>
+      </div>
+    )
   }
 
 }
--- client/src/components/Overview/ovComponents/galleryCarousel.jsx
+++ client/src/components/Overview/ovComponents/galleryCarousel.jsx
@@ -3,15 +3,40 @@ import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 var GalleryCarousel = (props) => {
+  //props.firstWindowPic is the current state
+  //props.setWindowPic sets the new state
+  //props.urlArray is the total array of URLS
 
-  console.log(props)
+  let func = props.setWindowPic;
+
+  let leftShift = (e) => {
+    var currentIndex = props.urlArray.indexOf(props.firstWindowPic)
+    if (currentIndex === 0) {
+      func(props.urlArray[props.urlArray.length - 1])
+    } else {
+      func(props.urlArray[currentIndex - 1]);
+    }
+  }
+
+  let rightShift = (e) => {
+    var currentIndex = props.urlArray.indexOf(props.firstWindowPic)
+    if (currentIndex === props.urlArray.length - 1) {
+      func(props.urlArray[0])
+    } else {
+      func(props.urlArray[currentIndex + 1])
+    }
+  }
+
 
   // add return/render here
     return (
-      props === undefined ?
+      props.urlArray.length === 0 ?
       <div>Loading the Product</div> :
 
-      <div><img className="mainPic" src={props.firstWindowPic} onClick={(e) => window.open(props.firstWindowPic)}></img></div>
+      <div><button onClick={leftShift}>LEFT</button>
+      <button onClick={rightShift}>RIGHT</button>
+      <img className="mainPic" src={props.firstWindowPic} onClick={(e) => window.open(props.firstWindowPic)}></img>
+      </div>
     )
   }
 
